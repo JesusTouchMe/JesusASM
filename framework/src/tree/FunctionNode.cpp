23506f48a321d6cbd48e53dc11fbd8e009646c8c
JesusTouchMe <dodoakmishot@gmail.com>
2025-04-22 17:40:25 +0200

    Set local count to argument count in FunctionNode

--- framework/src/tree/FunctionNode.cpp
+++ framework/src/tree/FunctionNode.cpp
@@ -35,6 +35,66 @@ namespace JesusASM::tree {
 
         if (!(modifiers & MODULEWEB_FUNCTION_MODIFIER_NATIVE)) {
             moduleweb::InsnList insnBuilder(builder.getModule());
+
+            int arguments = 0;
+
+            size_t i = 1;
+            while (i < descriptor.size() && descriptor[i] != ')') {
+                char c = descriptor[i];
+
+                switch (c) {
+                    case 'B':
+                    case 'S':
+                    case 'I':
+                    case 'C':
+                    case 'Z':
+                    case 'F':
+                        arguments += 1;
+                        i++;
+                        break;
+
+                    case 'L':
+                    case 'D':
+                        arguments += 2;
+                        i++;
+                        break;
+
+                    case 'R': {
+                        size_t semicolon = descriptor.find(';', i);
+                        if (semicolon == std::string_view::npos) {
+                            throw std::invalid_argument("Unterminated object type in descriptor");
+                        }
+
+                        arguments += 2;
+                        i = semicolon + 1;
+
+                        break;
+                    }
+
+                    case '[': {
+                        while (descriptor[i] == '[')
+                            i++;
+
+                        if (descriptor[i] == 'R') {
+                            size_t semicolon = descriptor.find(';', i);
+                            if (semicolon == std::string_view::npos)
+                                throw std::invalid_argument("Unterminated array object type in descriptor");
+                            i = semicolon + 1;
+                        } else {
+                            i++;
+                        }
+                        arguments += 2;
+
+                        break;
+                    }
+
+                    default:
+                        throw std::invalid_argument(std::string("Unexpected character in descriptor: ") + c);
+                }
+            }
+
+            insnBuilder.setLocalIndex(arguments);
+            
             instructions.emit(insnBuilder);
             insnBuilder.build(attributeBuilder);
         }
